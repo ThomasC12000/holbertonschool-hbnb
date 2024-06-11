d77c20aca84f9c34b11116c525386ede9dda37e8
Iaskasan <nokiesan@yahoo.fr>
2024-06-11 15:46:59 +0200

    Updated everything

--- API/user_management_endpoints.py
+++ API/user_management_endpoints.py
@@ -2,10 +2,8 @@ from flask import Flask
 from flask import jsonify
 from flask import request, abort
 from Persistence.data_manager import data_manager
-from Model.classes import User, Place
-from Model.base_class import BaseClass
+from Model.classes import User
 import json
-
 
 users = {}
 
@@ -18,19 +16,30 @@ def create_user():
         abort(400, description="Missing required fields: email, first_name, last_name, password")
     if not isinstance(data["email"], str) or "@" not in data["email"]:
         abort(400, description="Invalid email format")
-    
+
     entity: User = data_manager.create("User", **data)
+    save_data(data)
     # Respond with the newly created user
     return jsonify({'id': entity.id}), 201  # HTTP status 201 for Created
 
+def save_data(data):
+    with open("Persistence/users.json", 'w') as f:
+        json.dump(data, f)
+
+def load_data(filename=None):
+    try:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    except:
+        print("je suis dans l'except")
+        return {}
 
 @app.route("/users", methods=["GET"])
 def get_users():
     all_users = data_manager.get_by_class("User")
-    print(all_users)
     if all_users is None:
         abort(404, description="No users found")
-    #ici
-    return jsonify({ user.to_dict() for user in all_users })
+
+    return jsonify(load_data("Persistence/users.json"))
 
 app.run()
--- Persistence/data_manager.py
+++ Persistence/data_manager.py
@@ -2,6 +2,7 @@ from Model.base_class import BaseClass
 from Model.classes import Place, User, Review, Amenity, City, Country
 from Persistence.persistence_manager import IPersistenceManager
 from typing import Dict
+import json
 
 classes = {
     "Place": Place,
@@ -13,11 +14,19 @@ classes = {
 }
 
 class DataManager(IPersistenceManager):
-    def __init__(self):
-        self.storage: Dict[str, Dict[str, BaseClass | Place]] = {}
+    def __init__(self, storage_file="data.json"):
+        self.storage_file = storage_file
+        self.data = self.load_data()
+
+    def load_data(filename=None):
+        try:
+            with open(filename, 'r') as f:
+                return json.load(f)
+        except:
+            return {}
 
     def exists(self, entity_id, entity_type):
-        cls_storage = self.storage.get(entity_type)
+        cls_storage = self.data.get(entity_type)
         if (cls_storage):
             return cls_storage.get(entity_id) is not None
         return False
@@ -28,9 +37,9 @@ class DataManager(IPersistenceManager):
             entity = cls(*args, **kwargs)
             if (self.exists(entity.id, cls_name)):
                 raise Exception("Entity already exists")
-            if (self.storage.get(cls_name) is None):
-                self.storage[cls_name] = {}
-            self.storage[cls_name][entity.id] = entity
+            if (self.data.get(cls_name) is None):
+                self.data[cls_name] = {}
+            self.data[cls_name][entity.id] = entity
             return entity
         except Exception as e:
             print(e)
@@ -40,22 +49,22 @@ class DataManager(IPersistenceManager):
         entity_type = type(entity).__name__
         entity_id = entity.id
         if (self.exists(entity_id, entity_type)):
-            self.storage[entity_type][entity_id].save()
+            self.data[entity_type][entity_id].save()
 
     def get(self, entity_id, entity_type):
-        if entity_type in self.storage and entity_id in self.storage[entity_type]:
-            return self.storage[entity_type][entity_id]
+        if entity_type in self.data and entity_id in self.data[entity_type]:
+            return self.data[entity_type][entity_id]
         return None
 
     def get_by_class(self, cls_name):
-        return self.storage.get(cls_name)
+        return self.data.get(cls_name)
 
     def update(self, entity):
         entity_type = type(entity).__name__
         entity_id = entity.id
-        self.storage[entity_type][entity_id].update()
+        self.data[entity_type][entity_id].update()
 
     def delete(self, entity_id, entity_type):
-            del self.storage[entity_type][entity_id]
+            del self.data[entity_type][entity_id]
 
 data_manager = DataManager()
